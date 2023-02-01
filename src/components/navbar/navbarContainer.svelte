a10b49b950db0f6256b500953534d2463422b4df
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-01 13:48:50 +0530

    initial commit

---
+++ src/components/navbar/navbarContainer.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { metaStore } from "../../store/metaStore";
+	import NavbarMobile from "./navbarMobile.svelte";
+    import NavbarDesktop from "./navbarDesktop.svelte";
+
+</script>
+
+{#if $metaStore.screen === 'mobile' }
+    <NavbarMobile />
+{:else}
+    <NavbarDesktop />
+{/if}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.css";
+    import Navbar from "../components/navbar/navbarContainer.svelte";
+    import { metaStore } from "../store/metaStore";
+
+    $: innerWidth = 0
+    $: metaStore.set({ screen: innerWidth < 800 ? 'mobile' : 'desktop' })
+
+</script>
+<svelte:window bind:innerWidth />
+
+<div class="page-container">
+    <div class="w-screen bg-blue-50">
+        <Navbar />
+    </div>
+    <slot />
+</div>

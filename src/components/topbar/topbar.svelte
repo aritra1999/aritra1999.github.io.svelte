5453179587775f91a96a5f809ce1bbfebdca5d95
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-06 13:06:41 +0530

    layout change

---
+++ src/components/topbar/topbar.svelte
@@ -0,0 +1,11 @@
+<script>
+
+    let topbarItems = ['Primary', 'Social', 'Promotions', 'Updates', 'Forums'];
+</script>
+<div class="flex bg-white rounded-lg space-x-8">
+    {#each topbarItems as item}
+        <div class="flex items-center justify-center h-10 px-4 text-sm font-medium text-gray-500 border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300">
+            {item}
+        </div>
+    {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script>
     import "../app.css";
     import Navbar from "../components/navbar/navbarContainer.svelte";
+	import Topbar from "../components/topbar/topbar.svelte";
     import { metaStore } from "../store/metaStore";
 
     $: innerWidth = 0
-    $: metaStore.set({ screen: innerWidth < 800 ? 'mobile' : 'desktop' })
+    $: metaStore.set({ screen: innerWidth < 800 ? 'mobile' : 'desktop', showSidebar: true })
 
 </script>
 <svelte:window bind:innerWidth />
 
 <div class="page-container">
-    <div class="w-screen ">
+    <div class="w-screen relative">
         <Navbar />
     </div>
-    <slot />
+    <div class="py-2 pr-2 ease-in-out transition-all duration-300 {$metaStore.showSidebar && $metaStore.screen == 'desktop' ? 'pl-72' : 'pl-2'} ">
+        <Topbar />
+        <slot />
+    </div>
 </div>

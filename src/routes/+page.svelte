49e6dd47053bf6508e8da280e3cd9146938720c2
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-08 12:05:07 +0530

    added default page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-this is default page
+<div class="mt-20 flex w-full justify-center items-center">
+    <div>
+        No conversations selected. 
+        <div class="mt-20 text-slate-400 flex justify-center">
+            Have a good day 😃. 
+        </div>
+    </div>
+</div>

efe64272aa713e05da0ce70fd27f0bfa9d3d814c
Aritra Mondal <aritra041999.dev@gmail.com>
2023-02-13 11:33:45 +0530

    added papers page

--- src/routes/papers/+page.svelte
+++ src/routes/papers/+page.svelte
@@ -1,1 +1,14 @@
-this is papers page
+<script lang="ts">
+    import papers from './papers.json';
+    import PLCard from '../../components/plCard/PLCard.svelte';
+</script>
+
+<div class="lg:px-16 md:px-6 px-6 py-10">
+    <div class="columns-1 md:columns-2 xl:columns-3 gap-6">
+        {#each papers as paper}
+            <div class="inline-block w-full">
+                <PLCard item={paper} />
+            </div>
+        {/each}
+    </div>
+</div>

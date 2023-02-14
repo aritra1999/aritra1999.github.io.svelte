d44fabb7b3b2ed15a5e935706f8d05f6c5595426
Aritra Mondal <aritra041999.dev@gmail.com>
2023-02-14 09:15:01 +0530

    fixed header and link underline issue

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="lg:px-16 md:px-6 px-6 py-10">
-    <h2 class="magical-underline text-3xl font-semibold mb-8">
+    <h2 class="magical-underline text-3xl font-semibold mb-8 w-fit">
         Projects
     </h2>
     <div class="columns-1 md:columns-2 xl:columns-3 gap-4">

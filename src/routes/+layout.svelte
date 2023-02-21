4d0ca96b7a0086fa9b2f2430f1b34c3acf02859e
Aritra Mondal <aritra041999.dev@gmail.com>
2023-02-21 12:04:58 +0530

    Made email list component height responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
     <div class="fixed w-full pr-2 pb-3 ease-in-out transition-all duration-300 {($metaStore.showSidebar && $metaStore.screen === 'desktop') ? 'pl-72' : 'pl-2'} h-[calc(100vh-5rem)]">
         <div class="bg-white rounded-xl h-full">
             {#if $metaStore.screen == 'desktop' }<Topbar />{/if}
-            <div class="flex h-[calc(100vh-10rem)] overflow-y-auto w-full border-slate-200">
+            <div class="flex sm:h-[calc(100vh-10rem)] h-[calc(100vh-6rem)] overflow-y-auto overflow-x-hidden w-full border-slate-200">
                 <div class="{$metaStore.screen === 'desktop' ? 'border-r border-slate-200 w-[40rem]': 'w-full'} overflow-y-auto"><EmailList/></div>
                 {#if $metaStore.screen === 'desktop' || $page.url.pathname !== '/'}
                     <div class="{$metaStore.screen !== 'desktop' ? 'absolute left-0 w-screen h-[calc(100vh-5rem)]' : 'w-full'} p-2 bg-white z-50 overflow-y-auto">

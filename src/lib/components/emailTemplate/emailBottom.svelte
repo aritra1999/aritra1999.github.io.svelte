5f03f0e7ca5a12cc8fd1eddf3a28bc1858afe827
Aritra Mondal <aritra041999.dev@gmail.com>
2023-02-22 10:45:18 +0530

    Added email top and bottom template

---
+++ src/lib/components/emailTemplate/emailBottom.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+{#if $page.url.pathname !== '/'}
+    This is bottom
+{/if}
--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -8,7 +8,7 @@
 <div class="pb-24 pt-4 lg:px-16 md:px-6 px-2">
     <div class="my-4">
         <span class="magical-underline text-3xl font-semibold mb-4">
-            Work Experience
+            Experience
         </span>
         <div class="p-4 md:columns-2 columns-1 gap-4">
             {#each experiences as experience}

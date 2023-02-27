cffe6102d1bcb71dcddfce776fa06671872b1375
Aritra Mondal <aritra041999.dev@gmail.com>
2023-02-27 22:05:52 +0530

    Added contact form

--- src/lib/store/metaStore.ts
+++ src/lib/store/metaStore.ts
@@ -3,5 +3,5 @@ import { writable } from "svelte/store";
 export const metaStore = writable({
     screen: "desktop",
     showSidebar: true,
-    showContactForm: true
+    showContactForm: false
 });
